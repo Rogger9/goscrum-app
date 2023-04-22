a8b7f4f56fc2581581ecbae242d015f34b51ae24
Rogelio Gutierrez <gutierrez.rog12@gmail.com>
2023-04-22 12:23:32 -0300

    chore: add config for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
@@ -5,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['src/setupTest.ts'],
   },
 })
